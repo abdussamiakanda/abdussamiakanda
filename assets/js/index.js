196096963cb7c2e3c8123480371e0f07389c8cde
Md. Abdus Sami Akanda <abdussamiakanda@gmail.com>
2021-09-21 20:06:06 +0600

    majority done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,18 @@
 function goTo(path){
   window.location.assign(path);
 }
+
+var social = [
+  'https://www.researchgate.net/profile/Md_Akanda2',
+  'https://scholar.google.com/citations?user=hCntcSgAAAAJ&hl=en',
+  'https://www.linkedin.com/in/md-abdus-sami-akanda/',
+  'https://orcid.org/0000-0002-6742-2158',
+  'https://github.com/abdussamiakanda',
+  'https://www.facebook.com/mdabdussami.akanda/',
+  'https://www.youtube.com/channel/UC7yhrEJBWA5JTERVBmWNRUQ?view_as=subscriber',
+  'https://soundcloud.com/abdussamiakanda'
+]
+
+function goNow(no){
+  window.open(social[no], '_blank').focus();
+}
