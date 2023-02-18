1f70aa6e5bb4a0108e153268c70de06538a9c839
abdussamiakanda <abdussamiakanda@gmail.com>
2023-02-18 11:55:35 +0600

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,7 +21,7 @@ function hide(){
 var social = [
   'https://www.researchgate.net/profile/Md_Akanda2',
   'https://scholar.google.com/citations?user=hCntcSgAAAAJ&hl=en',
-  'https://www.linkedin.com/in/md-abdus-sami-akanda/',
+  'https://www.linkedin.com/in/abdussamiakanda/',
   'https://orcid.org/0000-0002-6742-2158',
   'https://github.com/abdussamiakanda',
   'https://www.facebook.com/mdabdussami.akanda/',
