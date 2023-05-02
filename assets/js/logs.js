ec9893bb712941468e1d23b0dd957d4f0e40635a
abdussamiakanda <abdussamiakanda@gmail.com>
2023-05-03 02:43:26 +0600

    .

--- assets/js/logs.js
+++ assets/js/logs.js
@@ -377,7 +377,7 @@ function processRender(id) {
   textArea.addEventListener("input", function (e) {
     this.style.height = "auto";
     this.style.height = this.scrollHeight + "px";
-    if (textArea.selectionEnd === textArea.value.length && !(window.innerHeight + window.scrollY >= document.body.offsetHeight)) {
+    if (textArea.selectionEnd === textArea.value.length) {
       window.scrollTo(0, document.body.scrollHeight);
     }
   });
